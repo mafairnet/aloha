de3762905af7092a0f5b6711df9959b082de4c6d
Miguel Torres <miguel@maf.mx>
2017-10-13 17:03:05 -0500

    Fisx db script
    Fisx db script

--- database/aloha-db-install.sql
+++ database/aloha-db-install.sql
@@ -15,6 +15,10 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+CREATE DATABASE IF NOT EXISTS aloha;
+
+USE aloha;
+
 --
 -- Table structure for table `country`
 --
